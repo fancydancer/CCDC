c8bc58fc4182ab0859de517bc7b53f586751c04f
JoshL <26966954+DLJ42@users.noreply.github.com>
2021-01-16 13:30:45 -0500

    update mysql_conf.sql
    remove "@'localhost'" from script when creating user "ise"

--- ccdc-injectEngine/sql_conf.sql
+++ ccdc-injectEngine/sql_conf.sql
@@ -1,6 +1,6 @@
 USE ccdc_ise;
 INSERT INTO Category VALUES ('0', 'ISE Manager Admin', 'Fri, 31 May 2019 18:07:50 -0400', 'Fri, 31 May 2019 19:07:50 -0400', 0);
 INSERT INTO Team (group_id, team_name, team_password, team_score, creation_date) VALUES('0', 'Amanda', 'duck', 0, 'Fri, 31 May 2019 19:07:50 -0400');
-CREATE USER 'ise'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'ise' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON ccdc_ise.* TO 'ise'@'localhost';
 FLUSH PRIVILEGES;
