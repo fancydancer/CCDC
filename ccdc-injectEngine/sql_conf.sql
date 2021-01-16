d51c4bf017392e8e38889ea8f17065327e8bd746
JoshL <26966954+DLJ42@users.noreply.github.com>
2021-01-16 13:28:25 -0500

    sql_conf updates
    add working database "ccdc_ise"

--- ccdc-injectEngine/sql_conf.sql
+++ ccdc-injectEngine/sql_conf.sql
@@ -1,3 +1,4 @@
+USE ccdc_ise;
 INSERT INTO Category VALUES ('0', 'ISE Manager Admin', 'Fri, 31 May 2019 18:07:50 -0400', 'Fri, 31 May 2019 19:07:50 -0400', 0);
 INSERT INTO Team (group_id, team_name, team_password, team_score, creation_date) VALUES('0', 'Amanda', 'duck', 0, 'Fri, 31 May 2019 19:07:50 -0400');
 CREATE USER 'ise'@'localhost' IDENTIFIED BY 'password';
